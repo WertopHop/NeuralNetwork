aa02e7f32e652049f14124b876751923545d1e30
WertopHop <135383976+WertopHop@users.noreply.github.com>
2025-10-11 15:52:00 +0300

    tokenize

---
+++ LTSM/LTSM.py
@@ -0,0 +1,20 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+
+
+class LTSM(nn.Module):
+    def __init__(self):
+        self.LSTM_dict = {}
+
+
+
+    def tokenize(self, text):
+        text_tokens = np.array([])
+        text_words = np.array(text.split())
+        for word in text_words:
+            if word not in self.LSTM_dict:
+                self.LSTM_dict[word] = len(self.LSTM_dict)
+            text_tokens = np.append(text_tokens, self.LSTM_dict[word])
+        return text_tokens
