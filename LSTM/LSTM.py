78c3f0e05cacc37e24811fb1350e2cf8f5d94b90
WertopHop <wertophop@mail.ru>
2025-10-13 17:30:54 +0300

    pre-version

--- LSTM/LSTM.py
+++ LSTM/LSTM.py
@@ -28,18 +28,24 @@ def tokenize(text, LSTM_dict):
     
 def main():
     LSTM_dict = {}
-    text = "This is a sample text for building the LSTM dictionary This text is for testing the LSTM model"
-    model = LSTMModel(vocab_size=4, embedding_dim=50, hidden_dim=100)
+    text = "Frogs are amazing amphibians that thrive in both aquatic and terrestrial environments. They begin their life as eggs in water, which hatch into tadpoles. As tadpoles grow, they develop legs and eventually jump onto land. Adult frogs are known for their distinctive croaks and play a vital role in ecosystems by controlling insect populations. Frogs have smooth, moist skin that helps them absorb water and breathe. They are found all over the world, from tropical rainforests to temperate regions. Some species of frogs are brightly colored, which can serve as a warning to predators about their toxicity. Frogs are also important indicators of environmental health, as they are sensitive to changes in their habitats."
     tokens, LSTM_dict = tokenize(text, LSTM_dict)
+    model = LSTMModel(vocab_size=len(LSTM_dict), embedding_dim=500, hidden_dim=1000)
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.AdamW(model.parameters(), lr=0.001)
-    input_sequence = torch.tensor([LSTM_dict[0], LSTM_dict[1]])  
-    output = model(input_sequence)
-    loss = criterion(output, torch.tensor([LSTM_dict[2]]))  # предсказываем следующее слово
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
-    print(f"Потеря: {loss.item():.4f}")
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+    for epoch in range(5):
+        for i in range(1, len(tokens) - 2):
+            text_tokens = np.array([])
+            for word in tokens[:-i]: 
+                text_tokens = np.append(text_tokens, word)
+            last_word = torch.tensor([tokens[-i]], dtype=torch.long) 
+            input_sequence = torch.tensor(text_tokens, dtype=torch.long).unsqueeze(1)
+            output = model(input_sequence)
+            loss = criterion(output, last_word)  # предсказываем следующее слово
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            print(f"loss: {loss.item():.4f}")
 
 
 if __name__ == "__main__":
