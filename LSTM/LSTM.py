4a5fdc2070dfa1a63a58dc76e78bb928197ad843
WertopHop <135383976+WertopHop@users.noreply.github.com>
2025-10-13 14:03:53 +0300

    learning

--- LSTM/LSTM.py
+++ LSTM/LSTM.py
@@ -5,24 +5,42 @@ import numpy as np
 
 
 class LSTMModel(nn.Module):
-    def __init__(self):
+    def __init__(self, vocab_size, embedding_dim, hidden_dim):
         super(LSTMModel, self).__init__()
-        self.LSTM_dict = {}
-        text = "This is a sample text for building the LSTM dictionary This text is for testing the LSTM model"
-        tokens = self.tokenize(text)
-        print("Tokens:", tokens)
+        self.embedding = nn.Embedding(vocab_size, embedding=embedding_dim)
+        self.lstm = nn.LSTM(embedding_dim, hidden_dim)
+        self.fc = nn.Linear(hidden_dim, vocab_size)
 
-    def tokenize(self, text):
-        text_tokens = np.array([])
-        text_words = np.array(text.split())
-        for word in text_words:
-            if word not in self.LSTM_dict:
-                self.LSTM_dict[word] = len(self.LSTM_dict)
-            text_tokens = np.append(text_tokens, self.LSTM_dict[word])
-        return text_tokens
+    def forward(self, input):
+        embedded = self.embedding(input)
+        output, (h_n, c_n) = self.lstm(embedded)
+        output = self.fc(output[-1])  
+        return output
+
+def tokenize(text, LSTM_dict):
+    text_tokens = np.array([])
+    text_words = np.array(text.split())
+    for word in text_words:
+        if word not in LSTM_dict:
+            LSTM_dict[word] = len(LSTM_dict)
+        text_tokens = np.append(text_tokens, LSTM_dict[word])
+    return text_tokens, LSTM_dict
     
 def main():
-    lstm = LTSM()
+    LSTM_dict = {}
+    text = "This is a sample text for building the LSTM dictionary This text is for testing the LSTM model"
+    model = LSTMModel(vocab_size=4, embedding_dim=50, hidden_dim=100)
+    tokens, LSTM_dict = tokenize(text, LSTM_dict)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.AdamW(model.parameters(), lr=0.001)
+    input_sequence = torch.tensor([LSTM_dict[0], LSTM_dict[1]])  
+    output = model(input_sequence)
+    loss = criterion(output, torch.tensor([LSTM_dict[2]]))  # предсказываем следующее слово
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+    print(f"Потеря: {loss.item():.4f}")
+
 
 if __name__ == "__main__":
     main()
